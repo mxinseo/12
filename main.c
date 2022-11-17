2b993481429fc68f2e5bfca4d6acc9d3cd00009b
rudalstj <jade201913@gmail.com>
2022-11-17 16:38:48 +0900

    01
    구현 시작

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	srand((unsigned)time(NULL));
+	
+	//opening
+	printf("=============================================\n");
+	printf("=               $ BINGO GAME $              =\n");
+	printf("=============================================\n\n\n");
+	
+	//game
+	//initialise bingo board
+	/*
+	while(game is not end) //줄 수가 N_BINGO보다 작음 
+	{
+		//bingo board print
+		//print no. of completed line
+		//select a number
+		//update the board state
+	}
+	*/
+	
+	//ending
+	printf("\n\n\n");
+	printf("=============================================\n");
+	printf("=       *^^*        BINGO!       *^^*       =\n");
+	printf("=               CONGRATURATION!             =\n");
+	printf("=       *^^*       YOU WIN!      *^^*       =\n");
+	printf("=============================================\n\n\n");
+	
+	
+	
+	return 0;
+}
